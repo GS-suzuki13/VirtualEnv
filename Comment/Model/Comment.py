45a3a2277f9b2a25e1a31bbb8d7a4eec0676efbe
GS-suzuki13 <gustavosuzuki3@gmail.com>
2020-02-17 08:58:25 -0300

    aa

---
+++ Comment/Model/Comment.py
@@ -0,0 +1,10 @@
+
+class Comment():
+
+    def __init__(self):
+        self.pessoa_id = 0
+        self.post_id = 0
+        self.conteudo = ''
+        self.dt_envio = ''
+
+    def __str__(self):
